3251993a24f1a9e93a5c3273fc3b8f067cd14e63
ravaldo <117010252+ravaldo@users.noreply.github.com>
2023-05-17 16:15:52 +0100

    tests complete

---
+++ src/python_functions_practice.py
@@ -0,0 +1,32 @@
+
+def return_10():
+	return 10
+	
+def add(x, y):
+	return x+y
+	
+def subtract(x, y):
+	return x-y
+	
+def multiply(x, y):
+	return x*y
+
+def divide(x, y):
+	return x/y
+	
+def length_of_string(mystring):
+	return len(mystring)
+
+def join_string(word1, word2):
+	return word1 + word2
+
+def add_string_as_number(num1, num2):
+	return int(num1) + int(num2)
+
+def number_to_full_month_name(num):
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	return months[num-1]
+	
+def number_to_short_month_name(num):
+	months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+	return months[num-1]
